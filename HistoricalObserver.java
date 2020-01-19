f42aa77ba227a2bd1989751b83cec084c518cf9c
Santeau <9491486+SanteauX@users.noreply.github.com>
2020-01-19 00:30:34 -0500

    Create HistoricalObserver.java
    Please do not violate the terms of use of Coinmarketcap, for educational purpose only thank you

---
+++ HistoricalObserver.java
@@ -0,0 +1,24 @@
+public class HistoricalObserver {
+  
+    public static void scrapeHistoricalCurrency(String url){
+      try {
+        int iterator = 1;
+        final Document coinMarketCapCurrencies = Jsoup.connect(url).get();
+        for(Element row: coinMarketCapCurrencies.select(".cmc-tab-historical-data tr"))
+        {	
+          if(iterator > 4) {
+            String date = row.select(".cmc-table__cell--sticky.cmc-table__cell").text();
+            String priceOpen = row.select(".cmc-table__cell:nth-of-type(2)").text();
+            String priceClose = row.select(".cmc-table__cell:nth-of-type(3)").text();
+            String dayHigh = row.select(".cmc-table__cell:nth-of-type(4)").text();
+            String dayLow = row.select(".cmc-table__cell:nth-of-type(5)").text();
+            String volumeTradedUSD = row.select(".cmc-table__cell:nth-of-type(6)").text();
+            String marketCapUSD =  row.select(".cmc-table__cell:nth-of-type(7)").text();
+          Historics Historic = new Historics("Ripple", date, priceOpen, priceClose, dayHigh, dayLow, volumeTradedUSD, marketCapUSD);
+          System.out.println(Historic.toString());
+            }
+          iterator++;
+          }
+        } catch(Exception exception) {exception.printStackTrace(); }	
+      }
+    }
